3c16e86757a5226c0aaed1ed5e879629b281a3e9
Александр <@LAPTOP-3OTD8BVV>
2022-03-24 02:40:38 +0300

    Обработка массива сообщений

--- src/index.js
+++ src/index.js
@@ -1,46 +1,180 @@
-import React from "react";
+/* eslint-disable eqeqeq */
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class ClassComponent extends React.Component {
-  constructor(props){
-    super(props)
+class MessageListClassComponent extends React.Component {
+  constructor(props) {
+    super(props);
     this.state = {
-      messageList: []
-    }
+      content: "",
+      count: 0,
+      messageList: [],
+    };
   }
 
-  addmessage = () => {
-    this.setState({
-      messageList: [...this.state.messageList, "newmessage"]
-    })
-  }
-
-  removemessage = (id) => {
-    this.setState({
-      films: this.state.films.filter(msg => msg !== id)
-    })
-  }
-
-  render() {
-    const {messageList} = this.state
-    console.log("this.state", this.state);
+  showmessages(message) {
     return (
       <div>
-        <h1>ClassComponent</h1>
+        <div>
+          <p>
+            <span>id: {message.id} </span>
+            <span>author: {message.author} </span>
+            <span>text: {message.text}</span>
+          </p>
+        </div>
+        <button onClick={() => this.removemessage(message)}>delete</button>
+      </div>
+    );
+  }
+
+  sendmessage = (message) => {
+    this.setState({
+      content: "",
+      messageList: [...this.state.messageList, message],
+      count: this.state.count + 1,
+    });
+  };
+
+  removemessage = (message) => {
+    this.setState({
+      messageList: this.state.messageList.filter(
+        (msg) => msg.id !== message.id
+      ),
+      count: this.state.count == 0 ? 0 : this.state.count - 1,
+    });
+  };
+
+  render() {
+    console.log("this.state", this.state);
+    const messages = this.state.messageList;
+    const currentid = this.state.count + 1;
+    const message = {
+      id: currentid,
+      author: "author" + currentid,
+      text: this.state.content,
+    };
+
+    return (
+      <div>
+        <h1>MessageListClassComponent</h1>
+        <input
+          value={this.state.content}
+          placeholder="ввод сообщения"
+          onChange={(e) =>
+            this.setState({
+              content: e.target.value,
+              messageList: [...this.state.messageList],
+              count: this.state.count,
+            })
+          }
+        />
         <hr />
 
-        <button onClick = {() => this.addmessage}>add</button>
-        {messageList.map(msg => (
-          <div>
-            <h3>{messageList}</h3>
-            <button onClick = {() => this.removemessage(msg)}>remove</button>
-          </div>
-        ))}
+        <button onClick={() => this.sendmessage(message)}>send</button>
+        <h2>Количество сообщений: {this.state.count}</h2>
+
+        {this.state.count == 0 ? (
+          <h2>Сообщений нет</h2>
+        ) : (
+          messages.map((msg) => this.showmessages(msg))
+        )}
       </div>
     );
   }
 }
+
+
+const MessageListFunComponent = ({ onClick }) => {
+  const [content, setContent] = useState("");
+  const [count, setCount] = useState(0);
+  const [messageList, setMessageList] = useState([]);
+  const [moving, setMoving] = useState(false);
+  console.log("count", count);
+  const messages = messageList;
+  const currentid = count + 1;
+  const message = {
+    id: currentid,
+    author: "author" + currentid,
+    text: content,
+  };
+
+  const showmessages = (message) => {
+    return (
+      <div>
+        <div>
+          <p>
+            <span>id: {message.id} </span>
+            <span>author: {message.author} </span>
+            <span>text: {message.text}</span>
+          </p>
+        </div>
+        <button onClick={() => removemessage(message)}>delete</button>
+      </div>
+    );
+  };
+
+  const sendmessage = (message) => {
+    setMessageList([...messageList, message]);
+    setCount((cnt) => cnt + 1);
+    setContent("");
+    setMoving(false);
+  };
+
+  const removemessage = (message) => {
+    setMessageList(messageList.filter((msg) => msg.id !== message.id));
+    setCount((cnt) => (cnt == 0 ? 0 : count - 1));
+    setMoving(true);
+  };
+
+  //Аналогично componentDidMount и componentDidUpdate:
+  useEffect(() => {
+    if (moving) { return; }
+
+    // Робот автоматически отправляет сообщение последнему пользователю
+    console.log("count", count);
+    if (count == 0) { return; }
+
+    console.log("messageList", messageList);
+    const recipient = messageList.find((msg) => msg.id == count).author;
+    if (recipient == "robot") { return;}
+
+    console.log("recipient", recipient);
+    let timerId = null;
+    const replyMessage = {
+      id: count + 1,
+      author: "robot",
+      text: "Ответ Робота",
+    };
+    console.log("replyMessage", replyMessage.text);
+    timerId = setTimeout(() => {
+      sendmessage(replyMessage);
+    }, 500);
+
+    return () => clearInterval(timerId);
+  });
+
+  return (
+    <div>
+      <h1>MessageListFunComponent</h1>
+      <input
+        value={content}
+        placeholder="ввод сообщения"
+        onChange={(e) => setContent(e.target.value)}
+      />
+      <hr />
+
+      <button onClick={() => sendmessage(message)}>send</button>
+      <h2>Количество сообщений: {count}</h2>
+
+      {count == 0 ? (
+        <h2>Сообщений нет</h2>
+      ) : (
+        messages.map((msg) => showmessages(msg))
+      )}
+    </div>
+  );
+};
 
 const CopyrightComponent = () => {
   return (
@@ -55,7 +189,10 @@ const CopyrightComponent = () => {
 const App = () => {
   return (
     <>
-      <ClassComponent />
+      <MessageListClassComponent />
+      <hr />
+      <MessageListFunComponent />
+      <hr />
       <CopyrightComponent />
     </>
   );
