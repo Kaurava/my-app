f3a1d4aefa33c79523493ad1d679be2c2596f100
Александр <aleckdev@yandex.ru>
2022-04-04 13:11:26 +0300

    Добавил темы и профиль

--- src/index.js
+++ src/index.js
@@ -1,35 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { MessageList, Layout, ChatList, Header } from "./components";
+import { Header } from "./components";
 import { ProfilePage, ChatPage } from "./pages";
+import { CustomThemeProvider } from "./theme-context";
+import { store } from "./store";
 
 import "./global.css";
 
-const App = () => {
-  return (
-    <>
-      <Layout
-        messages={<MessageList />}
-        chats={<ChatList />}
-        header={<Header />}
-      />
-    </>
-  );
-};
-
 ReactDOM.render(
   <React.StrictMode>
-      <BrowserRouter>
-        <Header />
+    <Provider store={store}>
+      <CustomThemeProvider>
+        <BrowserRouter>
+          <Header />
 
-        <Routes>
-          <Route path="/" element={<h1>Home page</h1>} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/chat/*" element={<ChatPage />} />
-          <Route path="*" element={<h1>404 page</h1>} />
-        </Routes>
-      </BrowserRouter>
+          <Routes>
+            <Route path="/" element={<h1>Home page</h1>} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/chat/*" element={<ChatPage />} />
+            <Route path="*" element={<h1>404 page</h1>} />
+          </Routes>
+        </BrowserRouter>
+      </CustomThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
